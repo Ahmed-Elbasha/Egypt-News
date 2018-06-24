c9506ce7a7f575cbe863456ed7a9bfdf940b381e
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-24 20:26:04 +0200

    Added ViewPager and TabLayout to Application

--- EgyNews/app/src/main/java/com/example/ahmedelbasha/egynews/MainActivity.java
+++ EgyNews/app/src/main/java/com/example/ahmedelbasha/egynews/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.ahmedelbasha.egynews;
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+        NewsArticleFragmentStatePagerAdapter adapter = new NewsArticleFragmentStatePagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setTabTextColors(Color.parseColor("#80ffffff"), Color.parseColor("#FFFFFF"));
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
     }
 }
---
+++ EgyNews/app/src/main/java/com/example/ahmedelbasha/egynews/NewsArticleFragmentStatePagerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.ahmedelbasha.egynews;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class NewsArticleFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+
+    public NewsArticleFragmentStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return  new BusinessFragment();
+        } else if (position == 1) {
+            return new EntertainmentFragment();
+        } else if (position == 2) {
+            return new PoliticsFragment();
+        } else if (position == 3) {
+            return  new SportsFragment();
+        } else {
+            return  new TechnologyFragment();
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return "آقتصاد";
+        } else if (position == 1) {
+            return "فن";
+        } else if (position == 2) {
+            return "سياسة";
+        } else if (position == 3) {
+            return "رياضة";
+        } else {
+            return "تكنولوجيا";
+        }
+    }
+}
